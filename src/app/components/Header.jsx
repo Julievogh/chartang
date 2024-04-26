a0d3b47eb7d994afc8b926b2215e8ecd5f0c450c
Julie Vogh <juvo0001@stud.kea.dk>
2024-04-26 09:42:48 +0200

    tekst margin

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -5,17 +5,18 @@ import Image from "next/image";
 export default function Header() {
   return (
     <header>
-      <Image src="/DiversaIcon.svg" alt="Logo" width={100} height={24} priority />{" "}
       <nav>
+        <Image src="/DiversaIcon.svg" alt="Logo" width={100} height={24} priority />{" "}
         <h1 className={styles.container}>Diversa</h1>
-
         <div className="links">
           <Link href={"/"}>Søg</Link>
           <Link href={"/pages/history/"}>Resultater</Link>
           <Link href="/pages/rules/">Regler</Link>
         </div>
       </nav>
-      <p>Tjek din hjemmeside</p>
+      <div className={styles.container2}>
+        <p>Tjek din hjemmeside</p>
+      </div>
     </header>
   );
 }
